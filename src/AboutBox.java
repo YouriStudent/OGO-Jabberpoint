d667f3ddf3425bcd40d1625fc1eeb0acdb8a8bfc
Youri <3141748-BeatRoy@users.noreply.gitlab.com>
2022-11-30 10:43:08 +0100

    Added newline

--- src/AboutBox.java
+++ src/AboutBox.java
@@ -12,7 +12,8 @@ public class AboutBox {
 						is freely copyable as long as you keep this notice and
 						the splash screen intact.
 						Copyright (c) 1995-1997 by Ian F. Darwin, ian@darwinsys.com.
-						Adapted by Gert Florijn (version 1.1) and Sylvia Stuurman (version 1.2 and higher) for the OpenUniversity of the Netherlands, 2002 -- now.
+						Adapted by Gert Florijn (version 1.1) and Sylvia Stuurman (version 1.2 and higher) for
+						the OpenUniversity of the Netherlands, 2002 -- now.
 						Author's version available from http://www.darwinsys.com/""",
 				"About JabberPoint",
 				JOptionPane.INFORMATION_MESSAGE
