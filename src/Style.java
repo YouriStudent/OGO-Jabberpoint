03c52f781ccca44847f99cc5f9a46f48fd162ceb
Youri <3141748-BeatRoy@users.noreply.gitlab.com>
2023-01-12 17:28:22 +0100

    Moved create styles to a Factory

--- src/Style.java
+++ src/Style.java
@@ -27,13 +27,7 @@ public class Style {
     }
 
     public static void createStyles() {
-        styles = new Style[5];
-        // De styles zijn vast ingecodeerd.
-        styles[0] = new Style(0, Color.red, 48, 20);    // style voor item-level 0
-        styles[1] = new Style(20, Color.blue, 40, 10);    // style voor item-level 1
-        styles[2] = new Style(50, Color.black, 36, 10);    // style voor item-level 2
-        styles[3] = new Style(70, Color.black, 30, 10);    // style voor item-level 3
-        styles[4] = new Style(90, Color.black, 24, 10);    // style voor item-level 4
+        styles = StyleFactory.generateDefaultStyles();
     }
 
     public static Style getStyle(int level) {
@@ -44,7 +38,7 @@ public class Style {
     }
 
     public String toString() {
-        return "[" + indent + "," + color + "; " + fontSize + " on " + leading + "]";
+        return String.format("[%s,$s; %s on %s]", indent, color, fontSize, leading);
     }
 
     public Font getFont(float scale) {
---
+++ src/StyleFactory.java
@@ -0,0 +1,19 @@
+import java.awt.*;
+import java.lang.reflect.Array;
+
+public class StyleFactory {
+    public static Style[] generateDefaultStyles() {
+        Style[] styles = new Style[5];
+        // style voor item-level 0
+        styles[0] = new Style(0, Color.red, 48, 20);
+        // style voor item-level 1
+        styles[1] = new Style(20, Color.blue, 40, 10);
+        // style voor item-level 2
+        styles[2] = new Style(50, Color.black, 36, 10);
+        // style voor item-level 3
+        styles[3] = new Style(70, Color.black, 30, 10);
+        // style voor item-level 4
+        styles[4] = new Style(90, Color.black, 24, 10);
+        return styles;
+    }
+}
