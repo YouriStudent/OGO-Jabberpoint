aca9b080ad599f0a78a3d45ddcb35b255828dd4f
Youri <3141748-BeatRoy@users.noreply.gitlab.com>
2023-01-15 21:36:27 +0100

    Added menu item names to a utility class

--- src/jabberpoint/controllers/MenuController.java
+++ src/jabberpoint/controllers/MenuController.java
@@ -8,6 +8,8 @@ import jabberpoint.factories.PresentationFactory;
 import javax.swing.*;
 import java.awt.*;
 
+import static jabberpoint.utility.MenuAttributes.*;
+
 /**
  * <p>The controller for the menu</p>
  *
@@ -15,19 +17,6 @@ import java.awt.*;
  * @version 1.6 2014/05/16 Sylvia Stuurman
  */
 public class MenuController extends MenuBar {
-
-    protected static final String ABOUT = "About";
-    protected static final String FILE = "File";
-    protected static final String EXIT = "Exit";
-    protected static final String GOTO = "Go to";
-    protected static final String HELP = "Help";
-    protected static final String NEW = "New";
-    protected static final String NEXT = "Next";
-    protected static final String OPEN = "Open";
-    protected static final String PAGENR = "Page number?";
-    protected static final String PREV = "Prev";
-    protected static final String SAVE = "Save";
-    protected static final String VIEW = "View";
     protected static final String TESTFILE = "testPresentation.xml";
     protected static final String SAVEFILE = "savedPresentation.xml";
 
---
+++ src/jabberpoint/utility/MenuAttributes.java
@@ -0,0 +1,16 @@
+package jabberpoint.utility;
+
+public class MenuAttributes {
+    public static final String ABOUT = "About";
+    public static final String FILE = "File";
+    public static final String EXIT = "Exit";
+    public static final String GOTO = "Go to";
+    public static final String HELP = "Help";
+    public static final String NEW = "New";
+    public static final String NEXT = "Next";
+    public static final String OPEN = "Open";
+    public static final String PAGENR = "Page number?";
+    public static final String PREV = "Prev";
+    public static final String SAVE = "Save";
+    public static final String VIEW = "View";
+}
